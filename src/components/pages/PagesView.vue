18cc464a8432dafcae73d88d4567697c789b9209
Dark_zarich <darkzarichv2@gmail.com>
2019-01-10 20:22:31 +0300

    added pagination for PagesView

--- src/components/pages/PagesView.vue
+++ src/components/pages/PagesView.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="row">
-    <div v-for="page in pages" :key="page.id" class="col-12 col-md-3">
-      <div>
-        <h3>{{page.title}}</h3>
-        <q>{{page.body}}</q>
-        <hr>
+  <div>
+    <div class="row">
+      <div v-for="page in pages" :key="page.id" class="col-12 col-md-3">
+        <div>
+          <h3>{{page.data.id}} ) {{page.data.title}}</h3>
+          <q>{{page.data.body}}</q>
+          <hr>
+        </div>
       </div>
+    </div>
+    <div class="row">
+      <b-pagination class="col-12" align="center" size="md" :total-rows="totalRows"
+                    @input="getPages" v-model="currentPage" :per-page="perPage">
+      </b-pagination>
     </div>
   </div>
 </template>
@@ -18,14 +25,26 @@ export default {
   data() {
     return {
       pages: [],
+      currentPage: 1,
+      perPage: 12,
+      totalRows: 100,
     };
   },
   created() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        this.pages = response.data;
+    this.getPages();
+  },
+  methods: {
+    getPages() {
+      const promises = [];
+      for (let i = ((this.currentPage - 1) * this.perPage) + 1;
+        i < ((this.currentPage - 1) * this.perPage) + this.perPage + 1
+        && i <= this.totalRows; i += 1) {
+        promises.push(axios.get(`https://jsonplaceholder.typicode.com/posts/${i}`));
+      }
+      Promise.all(promises).then((values) => {
+        this.pages = values;
       });
+    },
   },
 };
 </script>
