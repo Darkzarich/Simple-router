987dd4dc89055ea4ee3b60fc90763325ef3ef4cc
Dark_zarich <darkzarichv2@gmail.com>
2019-01-14 01:12:02 +0300

    build for gh-pages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Simple-router/',
+    assetsPublicPath: 'https://darkzarich.github.io/Simple-router/',
 
     /**
      * Source Maps
